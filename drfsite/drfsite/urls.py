7f10c336c9abed9422782107dccaf10ff8dbe2bd
Mike <mikhail.malishkin@gmail.com>
2025-02-16 21:22:48 +0400

    #9. Роутеры: SimpleRouter и DefaultRouter | Уроки по Django REST Framework

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -20,12 +20,27 @@ from django.urls import path, include
 from women.views import *
 from rest_framework import routers
 
-routers = routers.SimpleRouter()
-routers.register(r'women', WomenViewSet)
+class MyCustomRouter(routers.SimpleRouter):
+    routes = [
+        routers.Route(url=r'^{prefix}$',
+                      mapping={'get': 'list'},
+                      name='{basename}-list',
+                      detail=False,
+                      initkwargs={'suffix': 'List'}),
+        routers.Route(url=r'^{prefix}/{lookup}$',
+                      mapping={'get': 'retrieve'},
+                      name='{basename}-detail',
+                      detail=True,
+                      initkwargs={'suffix': 'Detail'})
+    ]
+
+router = routers.DefaultRouter() # MyCustomRouter()
+router.register(r'women', WomenViewSet, basename='women')
+print(router.urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(routers.urls))
+    path('api/v1/', include(router.urls))
     # path('api/v1/womenlist/', WomenViewSet.as_view({'get':'list'})),
     # path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put':'update'}))
 ]
