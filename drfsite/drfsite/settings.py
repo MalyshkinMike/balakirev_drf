a6892abef2a4ce292e0879e7fa4669dbad65b12a
Mike <mikhail.malishkin@gmail.com>
2025-02-20 11:00:08 +0400

    #12. Аутентификация по токенам. Пакет Djoser | Уроки по Django REST Framework

--- drfsite/drfsite/settings.py
+++ drfsite/drfsite/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'women.apps.WomenConfig',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser'
 ]
 
 MIDDLEWARE = [
@@ -132,5 +134,11 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
     ]
 }
